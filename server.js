805293fdc770ef6dbed5f2a2ddc15e2e910e04f9
Robert <rll@bu.edu>
2016-12-01 14:21:32 +0000

    server.js updated. JSON Pretty

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ app.get('/', function(req, res){
     res.end("Hello World");
 });
 
+app.get('/:dateTime', function(req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({
+        "first": req.params.dateTime,
+        "second": "This is a sample prop",
+        "third": "This time with both - " + req.params.dateTime
+    }, null, 3));
+})
+
 app.listen(8080, function(){
     console.log("Server running at 8080");
 });
