2a013626b07961be2f597fca72b4aa22401157d5
Robert <rll@bu.edu>
2016-12-02 00:03:31 +0000

    Finished version

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var app = express();
-var path = require('path');
-
 
 app.get('/', function(req, res){
-    res.end("Hello World");
+    res.sendFile('index.html', {root: __dirname});
 });
 
 // Grabs our passed in Date/Time
 app.get('/:dateTime', function(req, res) {
-    
-    var myDate = new Date();
-    var naturalDate = new Date(req.params.dateTime).toDateString();
-    var unixDate = new Date();
+    var input = req.params.dateTime;
+    var naturalDate;
+    var unixDate;
     
     res.setHeader('Content-Type', 'application/json');
+    
+    if(Number(input) > 0) {
+        unixDate = +input;
+        var naturalDate = new Date(input * 1000).toDateString();
+    } else if(new Date(input).toString() !== "Invalid Date") {
+        unixDate = new Date(input).getTime() / 1000;
+        naturalDate = new Date(input).toDateString();
+    } else {
+        unixDate = null;
+        naturalDate = null;
+    }
+    
     res.send(JSON.stringify({
         "unix": unixDate,
         "natural": naturalDate
